3160f934e5fc63e60c9a50802944cb3349e4c66b
Carson Koehnen <carson.koehnen@gmail.com>
2022-08-29 15:20:21 -0500

    added static mult

---
+++ static-mult/mod_info.lua
@@ -0,0 +1,13 @@
+name = "Statics Health Multiplier"
+uid = "b8d941e131multiplier"
+version = 1.1
+copyright = ""
+description = "All static units have health and shield points multiplied by number noted in config."
+author = "Balthazar (edit by Sad Turtle Man)"
+--icon = "/mods/BrewLAN_Gameplay/WaterGuard/mod_icon.dds"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+before = {  }
+after = {  }
